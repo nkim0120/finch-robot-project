71765a4a88e69054cd104ff10cd2d6b30da19b29
Nate <nkim0120@mymail.lausd.net>
2026-05-28 10:40:31 -0700

    day 2

---
+++ src/main/java/com/makePicture/CreatorFinch.java
@@ -0,0 +1,22 @@
+package com.makePicture;
+
+import com.birdbrain.Finch;
+
+public class CreatorFinch extends Finch {
+
+    public CreatorFinch(String device) {
+        super(device);
+    }
+
+    public void test() {
+        setMove("F", 10, 100);
+    }
+
+    public void navigate() {
+        
+    }
+
+    public void turnRightAndMove() {
+        
+    }
+}
---
+++ src/main/java/com/makePicture/TesterFinch.java
@@ -0,0 +1,12 @@
+package com.makePicture;
+
+public class TesterFinch {
+    
+    public static void main(String [] args) {
+        
+        CreatorFinch bird = new CreatorFinch("A");
+
+        bird.test();
+    }
+
+}
