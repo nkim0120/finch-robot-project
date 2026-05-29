a38b3f90d0e140d9266365524b30eaa9c7f0ec16
Nate <nkim0120@mymail.lausd.net>
2026-05-29 10:40:48 -0700

    day 3

--- src/main/java/com/makePicture/CreatorFinch.java
+++ src/main/java/com/makePicture/CreatorFinch.java
@@ -13,10 +13,41 @@ public class CreatorFinch extends Finch {
     }
 
     public void navigate() {
-        
+        int i = 0;
+        while (i == 0) {
+            if (canMove()) {
+                moveForward();
+                scootBack();
+            } else {
+                setTurn("L", 90, 100);
+                if (canMove()) {
+                    moveForward();
+                    scootBack();
+                } else {
+                    setTurn("L", 180, 100);
+                    if (canMove()) {
+                        moveForward();
+                        scootBack();
+                    } else {
+                        i++;
+                    }
+                }
+            }
+        }
     }
 
-    public void turnRightAndMove() {
-        
+    public void moveForward() {
+        setMove("F", getDistance() - 4, 100);
     }
+
+    public boolean canMove() {
+        return getDistance() > 12;
+    }
+
+    public void scootBack() {
+        if (getDistance() < 3) {
+            setMove("B", 4, 100);
+        }
+    }
+
 }
--- src/main/java/com/makePicture/TesterFinch.java
+++ src/main/java/com/makePicture/TesterFinch.java
@@ -6,7 +6,7 @@ public class TesterFinch {
         
         CreatorFinch bird = new CreatorFinch("A");
 
-        bird.test();
+        bird.scootBack();
     }
 
 }
